223400b008396002ec7dc0d7e2c784fdf0417d4d
zhang1998 <2764207312@qq.com>
2018-02-07 17:36:05 +0800

    heh

---
+++ java/Helloword.java
@@ -0,0 +1,12 @@
+/*************************************************************************
+    > File Name: Helloword.java
+    > Author: ma6174
+    > Mail: ma6174@163.com 
+    > Created Time: 2017年12月25日 星期一 19时33分22秒
+ ************************************************************************/
+public class Helloword{
+	public static void main(int a)
+	{
+		System.out.print("hehe");
+	}
+}
---
+++ java/workspace/HelloWord/src/com/test/Hello/Hello.java
@@ -0,0 +1,19 @@
+package com.test.Hello;
+
+public class Hello {
+	public class Hehe{
+		public Hehe(){
+			
+		    System.out.println("Hello World");
+		}
+		 Hehe myhehe =  new Hehe();	 
+	}
+	
+	public static void main(String []args) {
+		Hello my = new Hello();
+		 Hello.Hehe myhehe = my. new Hehe();	
+		    
+	    }
+	 Hehe myhehe =  new Hehe();	
+}
+
